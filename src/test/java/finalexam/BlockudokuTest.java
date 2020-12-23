d414266434f86b681dda5b7ee9a2618d5b6789d2
alonso804 <alonsobs804@gmail.com>
2020-12-23 13:38:24 -0500

    Problems with Sonar, don't cover the tests :/

--- src/test/java/finalexam/BlockudokuTest.java
+++ src/test/java/finalexam/BlockudokuTest.java
@@ -1,7 +1,5 @@
 package finalexam;
 
-import finalexam.Blockudoku;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -34,5 +32,69 @@ public class BlockudokuTest {
         boolean temp = blockudoku.insertA(board, 4, 4);
 
         Assert.assertEquals(temp, true);
+        Assert.assertEquals(board[4][4], 1);
+    }
+
+    public void testInsertB(){
+        Blockudoku blockudoku = new Blockudoku();
+        int board[][] = blockudoku.generateBoard(9);
+        boolean temp = blockudoku.insertB(board, 4, 4);
+
+        Assert.assertEquals(temp, true);
+        Assert.assertEquals(board[4][4], 1);
+    }
+
+    public void testInsertC(){
+        Blockudoku blockudoku = new Blockudoku();
+        int board[][] = blockudoku.generateBoard(9);
+        boolean temp = blockudoku.insertC(board, 4, 4);
+
+        Assert.assertEquals(temp, true);
+        Assert.assertEquals(board[4][4], 1);
+    }
+
+    public void testInsertD(){
+        Blockudoku blockudoku = new Blockudoku();
+        int board[][] = blockudoku.generateBoard(9);
+        boolean temp = blockudoku.insertD(board, 4, 4);
+
+        Assert.assertEquals(temp, true);
+        Assert.assertEquals(board[4][4], 1);
+    }
+
+    public void testInsertE(){
+        Blockudoku blockudoku = new Blockudoku();
+        int board[][] = blockudoku.generateBoard(9);
+        boolean temp = blockudoku.insertE(board, 4, 4);
+
+        Assert.assertEquals(temp, true);
+        Assert.assertEquals(board[4][4], 1);
+    }
+
+    public void testInsertF(){
+        Blockudoku blockudoku = new Blockudoku();
+        int board[][] = blockudoku.generateBoard(9);
+        boolean temp = blockudoku.insertE(board, 4, 4);
+
+        Assert.assertEquals(temp, true);
+        Assert.assertEquals(board[4][4], 1);
+    }
+
+    public void testInsertG(){
+        Blockudoku blockudoku = new Blockudoku();
+        int board[][] = blockudoku.generateBoard(9);
+        boolean temp = blockudoku.insertE(board, 4, 4);
+
+        Assert.assertEquals(temp, true);
+        Assert.assertEquals(board[4][4], 1);
+    }
+
+    public void testInsertH(){
+        Blockudoku blockudoku = new Blockudoku();
+        int board[][] = blockudoku.generateBoard(9);
+        boolean temp = blockudoku.insertH(board, 4, 4);
+
+        Assert.assertEquals(temp, true);
+        Assert.assertEquals(board[4][4], 1);
     }
 }
